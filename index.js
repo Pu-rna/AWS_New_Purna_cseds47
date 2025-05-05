52b577313ede632df459beaa0f73574c0ed34632
Pu-rna <purnakundu04@gmail.com>
2025-05-05 14:27:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send("Hello Engineers")
+  res.send("Helloooo Engineers")
 })
 
 app.get('/courses', function (req, res) {
