d874a3e16e613c5ae42c2757e20daacd5ed77e60
Pu-rna <purnakundu04@gmail.com>
2025-05-05 14:26:22 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send("Hi Engineers")
+  res.send("Hello Engineers")
 })
 
 app.get('/courses', function (req, res) {
